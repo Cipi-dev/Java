ab54effb4b9f17647e134ff6f6a6b89e8f0973c6
Cipi-dev <cibi270703@gmail.com>
2026-06-02 20:53:56 +0530

    Merge remote and local repositories

---
+++ rectangle.java
@@ -0,0 +1,19 @@
+public class rectangle{
+
+     public static void main(String []args){
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=4;j++){
+                if(i==2 & j==2){
+                     System.out.print(" ");
+                     continue;
+                }
+                if(i==2 & j==3){
+                     System.out.print(" " + " "+" ");
+                     continue;
+                }
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+     }
+}
